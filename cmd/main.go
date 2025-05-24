e2d207e51cf20ea1e49b62bd3f00b358b54672ad
Anvin Alias <anvinkaippillil123@gmail.com>
2025-05-24 17:10:46 +0530

    feat: add config package to parse config.yaml and load config in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/Anvinalias/az-blob-downloader/config"
 )
 
 func main() {
-	fmt.Println("Hello az-blob-downloader")
+	if err := run(); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+}
+
+func run() error {
+	cfg, err := config.LoadConfig("config.yaml")
+	if err != nil {
+		return err
+	}
+	log.Printf("Blobname: %s", cfg.Storage.BlobName)
+	log.Printf("Download path: %s", cfg.Paths.DownloadPath)
+	log.Printf("Log path: %s", cfg.Paths.LogPath)
+
+	return nil
 }
