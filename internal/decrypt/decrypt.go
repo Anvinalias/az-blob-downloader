bb32d457840f9ed2a15ad01602e2b9d4386f02e1
Anvin Alias <anvinkaippillil123@gmail.com>
2025-05-24 23:43:15 +0530

    feat: add AES decryption logic in internal/decrypt and update main.go to use it

---
+++ internal/decrypt/decrypt.go
@@ -0,0 +1,47 @@
+package decrypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+)
+
+// DeriveKey creates a 32-byte key from passphrase using SHA-256
+func DeriveKey(passphrase string) []byte {
+	hash := sha256.Sum256([]byte(passphrase))
+	return hash[:]
+}
+
+// decryptAESGCM decrypts base64 encoded nonce+ciphertext using passphrase
+func DecryptAESGCM(encodedCiphertext, passphrase string) (string, error) {
+	key := DeriveKey(passphrase)
+	ciphertext, err := base64.StdEncoding.DecodeString(encodedCiphertext)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	
+	nonceSize := aesgcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return "", errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
