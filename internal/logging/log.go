bd3bc43d1804812b8569848633118a792952fad7
Anvin Alias <anvinkaippillil123@gmail.com>
2025-06-04 16:34:36 +0530

    feat: create seperate log file for each run

--- internal/logging/log.go
+++ internal/logging/log.go
@@ -5,11 +5,15 @@ import (
     "log"
     "os"
     "path/filepath"
+    "time"
+    "fmt"
 )
 
 // Setup initializes logging to both file and console
 func Setup(logDir string) (*os.File, error) {
-    logFilePath := filepath.Join(logDir, "app.log")
+    timestamp := time.Now().Format("150405") // HHMMSS (24-hour format)
+    logFileName := fmt.Sprintf("app_%s.log", timestamp)
+    logFilePath := filepath.Join(logDir, logFileName)
     logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
     if err != nil {
         return nil, err
